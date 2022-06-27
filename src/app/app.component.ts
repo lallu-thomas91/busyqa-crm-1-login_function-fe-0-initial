987a35dd0780053e4da3a543da75acd487fed71d
Lallu Thomas <lalrose.tget@gmail.com>
2022-06-27 09:22:49 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'busyqa-crm-1-login_function-fe-0-initial';
+}
